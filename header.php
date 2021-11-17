fd55f872edba1735cb7d4e9c4a27fff4838ed192
KuroeGame <cloe-13500@hotmail.fr>
2021-11-17 16:45:03 +0100

    Create header.php
    insert header

---
+++ header.php
@@ -0,0 +1,122 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-gris">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-around row" id="navbarSupportedContent">
+                <div class="col-lg-4">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown lesproduits">
+                            <a class="nav-link me-2" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <h2>Nos produits</h2>
+                            </a>
+                            <nav class="dropdown-menu produits" aria-labelledby="navbarDropdown">
+                                <div class="d-flex align-items-start nav3">
+                                    <div class="nav nav2 flex-column nav-pills me-3 border-end" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                                        <button class="nav-link active border-bottom" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true"><img src="images/dog.png" alt="chiens" width="50" height="50" class="img-fluid ms-5"></button>
+                                        <button class="nav-link border-bottom" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"><img src="images/cat.png" alt="chats" width="50" height="50" class="img-fluid ms-5"></button></button>
+                                        <button class="nav-link border-bottom" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false"><img src="images/guinea-pig.png" alt="rongeurs" width="50" height="50" class="img-fluid ms-5"></button></button>
+                                        <button class="nav-link border-bottom" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false"><img src="images/chameleon.png" alt="reptiles" width="50" height="50" class="img-fluid ms-5"></button></button>
+                                        <button class="nav-link border-bottom" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false"><img src="images/poisson.png" alt="poissons" width="50" height="50" class="img-fluid ms-5"></button></button>
+                                    </div>
+                                    <div class="tab-content" id="v-pills-tabContent">
+                                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                                            <h2 class="border-bottom color-orange">Chien</h2>
+                                            <a href="" title="alimentation chiens" target="">Alimentation pour chien</a><br/>
+                                            <a href="" title="jouets chiens" target="">Jouets pour chien</a><br/>
+                                            <a href="" title="soins chiens" target="">Les soins du chien</a><br/>
+                                            <a href="" title="accessoires chiens" target="">Accessoires pour chien</a><br/>
+                                            <a href="" title="sécurité chiens" target="">Sécurité du chien</a><br/>
+                                            <li class="text-end pe-3"><i class="fas fa-paw"></i></li>
+                                        </div>
+                                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                                            <h2 class="border-bottom color-orange">Chat</h2>
+                                            <a href="" title="Alimentation chats" target="">Alimentation pour chat</a><br/>
+                                            <a href="" title="Jouets chats" target="">Jouets pour chat</a><br/>
+                                            <a href="" title="soins chats" target="">Les soins du chat</a><br/>
+                                            <a href="" title="Accessoires chats" target="">Accessoires pour chat</a><br/>
+                                            <a href="" title="Sécurité chats" target="">Sécurité du chat</a><br/>
+                                            <li class="text-end pe-3"><i class="fas fa-paw"></i></li>
+                                        </div>
+                                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                                            <h2 class="border-bottom color-orange">Rongeur</h2>
+                                            <a href="" title="Alimentation rongeurs" target="">Alimentation pour rongeur</a><br/>
+                                            <a href="" title="Jouets rongeurs" target="">Jouets pour rongeur</a><br/>
+                                            <a href="" title="soins rongeurs" target="">Les soins du rongeurs</a><br/>
+                                            <a href="" title="Accessoires rongeurs" target="">Accessoires pour rongeur</a><br/>
+                                            <a href="" title="Abrit rongeurs" target="">Abrit du rongeur</a><br/>
+                                            <li class="text-end pe-3"><i class="fas fa-paw"></i></li>
+                                        </div>
+                                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                                            <h2 class="border-bottom color-orange">Reptile</h2>
+                                            <a href="" title="Alimentation reptile" target="">Alimentation pour reptile</a><br/>
+                                            <a href="" title="soins reptiles" target="">Les soins du reptile</a><br/>
+                                            <a href="" title="Accessoires reptiles" target="">Accessoires pour reptile</a><br/>
+                                            <a href="" title="Habitat reptiles" target="">Habitat du reptile</a><br/>
+                                            <li class="text-end pe-3"><i class="fas fa-paw"></i></li>
+                                        </div>
+                                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                                            <h2 class="border-bottom">Poisson</h2>
+                                            <a href="" title="Alimentation poisson" target="">Alimentation pour poisson</a><br/>
+                                            <a href="" title="Habitat poissons" target="">Habitat du poisson</a><br/>
+                                            <a href="" title="Soins poissons" target="">Les soins du poisson</a><br/>
+                                            <a href="" title="Plantes et décoration" target="">Plantes et décoration</a><br/>
+                                            <li class="text-end pe-3"><i class="fas fa-paw"></i></li>
+                                        </div>
+                                    </div>
+                                </div>
+                            </nav>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link me-2" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <h2>Promos & Marques</h2>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#"><h2 class="h2nav">Promos</h2></a></li>
+                                <li><a class="dropdown-item" href="#"><h2 class="h2nav">Marques</h2></a></li>
+                                <li class="text-end pe-3"><i class="fas fa-paw"></i></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <h2>Nos services</h2>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#"><h2 class="h2nav">Nous contacter</h2></a></li>
+                                <li><a class="dropdown-item" href="#"><h2 class="h2nav">La Boutiques</h2></a></li>
+                                <li><a class="dropdown-item" href="#"><h2 class="h2nav">Activités</h2></a></li>
+                                <li class="text-end pe-3"><i class="fas fa-paw"></i></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <a class="navbar-brand" href="index.php">
+                        <h1>
+                            <img src="images/logo1.png" alt="logo" width="134" height="100">
+                        </h1>
+                    </a>
+                </div>
+                <div class="col-lg-4 d-lg-flex justify-content-around text-center">
+                    <form class="form-inline">
+                        <div class="input-group">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Recherchez" aria-label="Search">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-dark my-2 my-sm-0" type="submit"><i class="fas fa-search fa-lg"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                    <a href="#" class="p-2"><i class="fas fa-shopping-bag fa-lg"></i></a>
+                    <a href="#" class="p-2"><i class="fas fa-user-alt fa-lg"></i></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- vide -->
+            <section>
+                <div class="container-fluid menu">
+                </div>
+            </section>
+</header>
