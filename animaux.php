37bd58cefcbc1e3630c7778557421957cdba28f6
KuroeGame <cloe-13500@hotmail.fr>
2021-11-17 16:45:20 +0100

    Create animaux.php
    insert animaux

---
+++ animaux.php
@@ -0,0 +1,39 @@
+<!-- les animaux-->
+            <section>
+                <div class="container text-center"> <!-- container ou fluid ? -->
+                    <h2>Retrouvez les univers de tous nos Animaux</h2>
+                    <div class="row"> <!-- align 12 colonnes -->
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-2">
+                                <a href="">
+                                <img src="images/chameleon.png" alt="image" width="100px" height="100px" class="img-fluid">
+                                <p>Reptiles <i class="fas fa-long-arrow-alt-right"></i></p>
+                            </a>
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="">
+                                <img src="images/cat.png" alt="image" width="100px" height="100px" class="img-fluid">
+                                <p>Reptiles <i class="fas fa-long-arrow-alt-right"></i></p>
+                            </a>
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="">
+                                <img src="images/dog.png" alt="image" width="100px" height="100px" class="img-fluid">
+                                <p>Reptiles <i class="fas fa-long-arrow-alt-right"></i></p>
+                            </a>
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="">
+                                <img src="images/guinea-pig.png" alt="image" width="100px" height="100px" class="img-fluid">
+                                <p>Reptiles <i class="fas fa-long-arrow-alt-right"></i></p>
+                            </a>
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="">
+                                <img src="images/poisson.png" alt="image" width="100px" height="100px" class="img-fluid">
+                                <p>Reptiles <i class="fas fa-long-arrow-alt-right"></i></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
