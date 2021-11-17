136516647d3d8981ef61fda6d2e66e90cef79e09
KuroeGame <cloe-13500@hotmail.fr>
2021-11-17 16:45:10 +0100

    Create footer.php
    insert footer

---
+++ footer.php
@@ -0,0 +1,144 @@
+<footer>
+
+<!-- engagement -->
+            <section>
+                <div class="bg-gris engagement">
+                    <div class="container-fluid text-center p-3"> <!-- container ou fluid ? -->
+                        <h2 class="p-3">Notre Engagement</h2>
+                        <div class="row"> <!-- align 12 colonnes -->
+                            <div class="col-lg-4 col-sm-6"> 
+                                <div class="d-flex justify-content-center align-items-center">
+                                    <i class="fas fa-euro-sign fa-5x"></i>
+                                    <p class="pt-3 ps-1">Remboursement sous <br/><strong class="color-orange">30 jours</strong></p>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6">
+                                <div class="d-flex justify-content-center align-items-center">
+                                    <i class="fas fa-comments fa-5x"></i>
+                                    <p class="pt-3 ps-1">Un service à votre écoute</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6">
+                                <div class="d-flex justify-content-center align-items-center">
+                                    <i class="fas fa-dolly fa-5x"></i>
+                                    <p class="pt-3 ps-1">Livraison <strong class="color-orange">Gratuite</strong> sous 24h<br/><em>sans minimum d'achat</em></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+<!-- footer-->
+            <section>
+                <div class="bg-bleu">
+                    <div class="container-fluid"> <!-- container ou fluid ? -->
+                        <div class="row p-4"> <!-- align 12 colonnes -->
+                            <div class="col-lg-4 col-sm-12 mt-3 mb-3 text-light">
+                                <div class="">
+                                    <h3>Nous Rejoindre</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies tincidunt turpis eget pulvinar massa pellentesque leo. Et, pellentesque nulla facilisis elit, viverra. Fermentum, arcu sagittis, nulla in sed arcu, ut sit molestie. Ultricies pulvinar cursus mauris, vestibulum nisi eu.</p>
+
+                                    <form class="row align-items-center">
+                                        <div class="col-12 pe-5 ps-5">
+                                            <label class="visually-hidden" for="inlineFormInputGroupUsername">e-mail</label>
+                                        <div class="input-group">
+                                            <div class="input-group-text"><i class="fas fa-envelope"></i></div>
+                                            <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Votre e-mail">
+                                            <div class="input-group-text "><button type="submit" class="btn"><i class="fas fa-chevron-right"></i></button></i></div>
+                                        </div>
+                                      </div>
+                                    </form>
+
+                                    <div class="row text-center mt-3 mb-3">
+                                        <div class="col-3">
+                                            <a href="" title="lien facebook" target="_blank">
+                                                <i class="fab fa-facebook fa-3x"></i>
+                                            </a>
+                                        </div>
+                                        <div class="col-3">
+                                            <a href="" title="lien instagram" target="_blank">
+                                                <i class="fab fa-instagram fa-3x"></i>
+                                            </a>
+                                        </div>
+                                        <div class="col-3">
+                                            <a href="" title="lien youtube" target="_blank">
+                                                <i class="fab fa-youtube fa-3x"></i>
+                                            </a>
+                                        </div>
+                                        <div class="col-3">
+                                            <a href="" title="lien pinterest" target="_blank">
+                                                <i class="fab fa-pinterest fa-3x"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="mt-4 mb-2 text-center">
+                                        <img src="images/avis.png" alt="avis" width="330" height="100" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-12 mt-3 mb-3">
+                                <div class="bg-orange rounded-3 p-3 questions">
+                                    <h3>Une questions ?</h3>
+                                    <p>Notre équipe nimoise sera toujours ravie de vous renseigner. Si vous avez une question sur un produit ou une commande, n’hésitez pas à nous contacter, nous sommes là pour vous aider !</p>
+                                    <h4>HEURES D'OUVERTURE</h4>
+                                    <p>Du lundi au vendredi de 9h à 13h et de 14h à 18h, et le samedi de 10h à 13h et de 14h à 18h.</p>
+                                    <div class="text-center">
+                                        <a href="tel:+33606060606" class="d-flex justify-content-center align-items-center">
+                                            <i class="fas fa-phone-alt"></i>
+                                             <p class="m-3">06 06 06 06 06</p>
+                                            <i class="fas fa-arrow-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-12 text-light mt-3 mb-3">
+                                <h3 class="mb-4">Nos Partenaires</h3>
+                                <div class="row"> <!-- align 12 colonnes -->
+                                    <div class="col-lg-6 col-sm-6 text-center mb-2">
+                                        <img src="images/logo-santevet.png" alt="santevet"  class="img-fluid" width="100" height="41">
+                                    </div> 
+                                    <div class="col-lg-6 col-sm-6 text-center mb-2">
+                                        <img src="images/Logo_Royal_Canin.png" alt="royal canin" class="img-fluid" width="100" height="31">
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6 text-center mb-2">
+                                        <img src="images/Pedigree-logopng.png" alt="pedigree"  class="img-fluid" width="100" height="63">
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6 text-center mb-2">
+                                        <img src="images/Pro_Plan_Logo.png" alt="pro plan" width="100"  class="img-fluid" height="60">
+                                    </div>
+                                   
+                                </div>
+                                <h3 class="mb-4 mt-4">Nos moyens de paiements</h3>
+                                <div class="row">
+                                    <div class="col-12 text-center">
+                                        <img src="images/Master-Card-icon.png" alt="image" width="50" height="35" class="img-fluid payments m-2">
+                                        <img src="images/paypal.png" alt="image" width="50" height="35" class="img-fluid payments m-2">
+                                        <img src="images/visa.png" alt="image" width="50" height="35" class="img-fluid payments m-2">
+                                        <img src="images/american-express.png" alt="image" width="50" height="35" class="img-fluid payments m-2">
+                                    </div>    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row p-4">
+                            <div class="col-lg-12 col-sm-12 text-center p-3 border-top">
+                                <a href="/" title="" target="" class="me-4">CGU</a>
+                                <a href="/" title="" target="" class="me-4">CGV</a>
+                                <a href="/" title="" target="" class="me-4">Mention légales</a>
+                                <a href="/" title="" target="" class="me-4">Contact</a>
+                                <a href="/" title="" target="">Nous trouver</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+            <script>
+             jQuery(function($) {
+            $('.produits button[data-bs-toggle="pill"]').click(function (e) {
+                e.stopPropagation()
+                $(this).tab('show')
+            })
+            });
+            </script>
+        </footer>
