80d1b4940918b0e1b87fb09631d6ff866bd742d1
KuroeGame <cloe-13500@hotmail.fr>
2021-11-22 09:54:20 +0100

    Create cgu.php

---
+++ cgu.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" type="text/css">
+<!-- Lier bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+
+<!-- Script JS -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<!-- intégration icon fontawesome -->
+        <script src="https://kit.fontawesome.com/6939106af0.js" crossorigin="anonymous"></script>
+        <title>La Nîmes'alerie</title>
+    </head>
+    <body>
+
+        <header>
+            <?php include("header.php");?>
+        </header>
+
+        <main>
+
+<!-- chemin d'acces-->
+            <section>
+                <div class="container-fluid"> <!-- container ou fluid ? -->
+                    <div class="row"> <!-- align 12 colonnes -->
+                        <div class="col-lg-12 col-sm-12">
+                            <div class="">
+                                <p>
+                                    <a href="index.php">Accueil</a>
+                                    -
+                                    <a href="">CGU</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+<!-- contenu-->
+            <section>
+                <div class="container"> <!-- container ou fluid ? -->
+                    <h2 class="ms-4 titre">Conditions Générales d'Utilisateur</h2>
+                    <div class="row"> <!-- align 12 colonnes -->
+                        <div class="col-lg-12 col-sm-12 pt-5 pb-5">
+                            <h3>1 - Généralités</h3>
+                            <p>Les présentes conditions générales d’utilisation s’appliquent à tous les utilisateurs du Site Internet suivant :  www.lanimesalerie.com<br/>
+                            <br/>
+                            La consultation dudit Site implique l’acceptation implicite par l’utilisateur des présentes conditions générales d’utilisation.<br/>
+                            <br/>
+                            Les informations contenues dans le site sont fournies à titre d’information générale, à titre indicatif et à titre promotionnel. Elles n’ont pas pour vocation de se substituer aux conseils ou à la consultation directement d’un professionnel de la santé animale.<br/>
+                            <br/>
+                            Le Grand Public aura la possibilité de se voir envoyer des Newsletters de la part de La Nîmes’alerie en communiquant son adresse électronique.<br/>
+                            <br/>
+                            Par ailleurs, La Nîmes’alerie précise que ses produits sont commercialisés exclusivement auprès de consommateurs français et belges.<br/>
+                            <br/>
+                            En cas de perte des identifiants, La Nîmes’alerie vous propose dans la rubrique « mot de passe oublié » d’indiquer votre adresse mail pour pouvoir récupérer votre mot de passe.<br/>
+                            <br/>                                                        
+                            La Nîmes’alerie se réserve le droit de modifier et de mettre à jour à tout moment et sans préavis l’accès au site ainsi que les conditions générales d’utilisation du site, lesquelles seront applicables dès leur apparition et ce, sans  pouvoir être tenue pour responsable des conséquences desdites modifications.</p>
+                            <h3>2 - Demande de fermeture de compte</h3>
+                            <p>Lorsque l’utilisateur demandera la clôture de son compte client, La Nîmes’alerie procédera à la désinscription du client à toutes les newsletters lanimesalerie.com, et à la suppression de son adresse mail dans son compte client.</p>
+                            <h3>3 - Responsabilité</h3>
+                            <p>Les informations contenues sur le Site<br/>
+                            Les informations diffusées sur le site www.lanimesalerie.com le sont à des fins notamment informatives. A cet effet, La Nîmes’alerie met tout en œuvre pour garantir la fiabilité, la licéité et la validité par rapport à la réglementation applicable mais ne donne aucune garantie à ce titre. Par conséquent, l’accès au site www.lanimesalerie.com, et l’utilisation de son contenu sont aux risques de l’utilisateur et La Nîmes’alerie ne saurait être tenue pour responsable des erreurs, d’une absence de disponibilité des informations et/ou de la présence de virus sur son Site Internet. Les informations communiquées sur le site ne sauraient en aucun cas remplacer ou se substituer à un conseil pris auprès d’un professionnel de la santé animale. De plus, La Nîmes’alerie vous permet de discuter soit avec son équipe, soit avec les visiteurs en ligne.<br/>
+                            <br/>
+                            L’utilisateur du site reconnait qu’il fait usage des informations du Site sous sa seule responsabilité, laquelle sera engagée à raison de tout préjudice, direct ou indirect, de quelque nature que ce soit.<br/>
+                            <br/>
+                            Les liens hypertextes<br/>
+                            Tout document accessible à partir de liens hypertextes sur le site ou dans les documents téléchargeables ne le sont que pour un usage privé, personnel et non transmissible.<br/>
+                            <br/>
+                            Toute consultation du Site Internet de tiers (ou de tout document accessible à partir de liens hypertextes) est effectuée sous la seule responsabilité de l’utilisateur et ne saurait en aucun cas engager la responsabilité de La Nîmes’alerie.<br/>
+                            <br/>
+                            Disponibilité-Maintenance<br/>
+                            La Nîmes’alerie pourra à tout moment, modifier ou interrompre de manière temporaire ou permanente, tout ou partie du Site pour effectuer des opérations de maintenance et/ou effectuer des améliorations, et/ou des modifications sur le Site sans pour autant être tenue responsable de ces modifications, suspensions ou interruptions du Site. A cet effet, la société La Nîmes’alerie ne fournit aucune garantie quant à la disponibilité ou l’accessibilité du Site.</p>
+                            <h3>4 - Propriété intellectuelle</h3>
+                            <p>La structure générale du Site, les textes, les articles, les images ou éléments graphiques, les données, les marques et logos, le nom de domaine, et tout autre élément composant le site web, constituent des œuvres protégées et sont la propriété exclusive de La Nîmes’alerie, ils ne peuvent en aucun cas être reproduits, modifiés, présentés ou utilisés à des fins d’intérêt public ou commercial. Tout contrevenant serait susceptible d’être poursuivi pour contrefaçon en application des dispositions des articles L.335-2 et suivants du Code de la propriété intellectuelle.<br/>
+                            <br/>
+                            Les marques et logos de la société La Nîmes’alerie, figurant sur le site sont des marques déposées. Elles sont la propriété exclusive de la Société ou lui sont concédés en licence.<br/>
+                            <br/>
+                            La Nîmes’alerie autorise l’utilisateur à visualiser, parcourir et à consulter les pages web du site www.lanimesalerie.com sur son ordinateur personnel au moyen d’un navigateur, à imprimer des extraits de ces pages ou encore à télécharger sur le disque dur de son ordinateur les informations ou autres documents mis à sa disposition sur le site et qui sont la propriété de La Nîmes’alerie. L’utilisateur ne sera autorisé qu’à détenir une copie de ces documents, pour un usage strictement privé, personnel et non transmissible.</p>
+                            <h3>5 - Informatique et libertés/protection des données personnelles</h3>
+                            <p>Conformément à la loi relative à l’informatique, aux fichiers et aux libertés du 6 janvier 1978, le site www.wanimo.com a fait l’objet des déclarations suivantes</p>
+                            <ul>
+                                <li>Déclaration du Site aupr ès de Monsieur le Procureur de la République, le 7 juin 2000. Récépissé du 10/07/2000, n°66/2000.</li>
+                                <li>Déclaration du Site auprès de Monsieur le Président du Conseil supérieur de l’Audiovisuel le 7 juin 2000. Récépissé de la déclaration en date du 29 juin 2000.</li>
+                                <li>Déclaration du site auprès de la Commission nationale informatique et libertés (CNIL), le 21 juin 2000. Récépissé de Déclaration n° 712283.</li>
+                                <li>Déclaration simplifiée de la gestion de fichiers auprès de la CNIL, le 18 janvier 2012. Récépissé de Déclaration n° 1559580.</li>
+                            </ul>
+                            <p>La Nîmes’alerie s’engage à respecter le caractère confidentiel des informations personnelles fournies par les utilisateurs du site internet.<br/>
+                            <br/>
+                            Certaines des données personnelles enregistrées sur notre serveur sont celles que vous fournissez volontairement lorsque vous remplissez les formulaires en ligne, et les autres sont recueillies automatiquement (URL, adresse IP, langue, type de navigateur utilisé, date et heure de la requête…).<br/>
+                            Afin de permettre l’utilisation de fonctions de personnalisation, il vous sera demandé de fournir des données personnelles (nom, adresse…). Vous êtes tout à fait libre de refuser de transmettre de telles données. Il est cependant possible que vous ne soyez pas en mesure d’accéder à certaines fonctions ou de recevoir les informations demandées.<br/>
+                            <br/>
+                            Conformément à la loi n°79-17 du 6 janvier 1978, dite loi informatique et libertés, vous disposez d’un droit de modification, rectification et de suppression des données vous concernant sur simple demande adressée par e-mail via la rubrique « Contacts »
+                            La Nîmes’alerie se réserve le droit de modifier à tout moment les règles de fonctionnement du site Internet susvisé.</p>
+                            <h3>6 - Les cookies</h3>
+                            <p>Un cookie est un petit fichier texte déposé sur votre ordinateur lors de la visite d'un site ou de la consultation d'une publicité. Il a notamment pour but de collecter des informations relatives à votre navigation sur les sites et de vous adresser des services personnalisés. Dans votre ordinateur les cookies sont gérés par votre navigateur internet.<br/>
+                            <br/>
+                            Vous disposez de différents moyens pour gérer les cookies.<br/>
+                            <br/>
+                            Le paramétrage de votre navigateur internet<br/>
+                            Vous pouvez à tout moment choisir de désactiver ces cookies. Votre navigateur peut également être paramétré pour vous signaler les cookies qui sont déposés dans votre ordinateur et vous demander de les accepter ou non. Vous pouvez accepter ou refuser les cookies au cas par cas ou bien les refuser systématiquement une fois pour toutes.<br/>
+                            <br/>
+                            Nous vous rappelons que le paramétrage est susceptible de modifier vos conditions d'accès à nos services nécessitant l'utilisation de cookies.<br/>
+                            Voici comment contrôler ou empêcher l'enregistrement des cookies :<br/>
+                            <br/>
+                            La configuration de chaque navigateur est différente. Elle est décrite dans le menu d'aide de votre navigateur, qui vous permettra de savoir de quelle manière modifier vos souhaits en matière de cookies. Vous pouvez désactiver les cookies en suivant les instructions comme suit :</p>
+                            <ol>
+                                <li>si vous utilisez le navigateur Internet Explorer<br/>
+                                Dans Internet Explorer, cliquez sur le bouton Outils, puis sur Options Internet.<br/>
+                                Sous l'onglet Général, sous Historique de navigation, cliquez sur Paramètres.<br/>
+                                Cliquez sur le bouton Afficher les fichiers.<br/>
+                                Cliquez sur l'en-tête de colonne Nom pour trier tous les fichiers dans l'ordre alphabétique, puis parcourez la liste jusqu'à ce que vous voyez des fichiers commençant par le préfixe » Cookie «. (tous les cookies possèdent ce préfixe et contiennent habituellement le nom du site Web qui a créé le cookie).<br/>
+                                Sélectionnez le ou les cookies comprenant le nom « www.lanimesalerie » et supprimez-les<br/>
+                                Fermez la fenêtre qui contient la liste des fichiers, puis cliquez deux fois sur OK pour retourner dans Internet Explorer.</li><br/>
+                                <li>si vous utilisez le navigateur Firefox<br/>
+                                Allez dans l'onglet » Outils « du navigateur puis sélectionnez le menu » Options «<br/>
+                                Dans la fenêtre qui s'affiche, choisissez » Vie privée « et cliquez sur » Affichez les cookies<br/>
+                                Dans la fenêtre qui s'affiche, choisissez » Vie privée « et cliquez sur » Affichez les cookies<br/>
+                                Repérez les fichiers qui contiennent le nom « www.lanimesalerie»Sélectionnez-les et supprimez-les.</li><br/>
+                                <li>si vous utilisez le navigateur Safari<br/>
+                                Dans votre navigateur, choisissez le menu Édition > Préférences.<br/>
+                                Cliquez sur Sécurité.<br/>
+                                Cliquez sur Afficher les cookies.<br/>
+                                Sélectionnez les cookies qui contiennent le nom « www.lanimesalerie » et cliquez sur Effacer ou sur Tout effacer.<br/>
+                                Après avoir supprimé les cookies, cliquez sur Terminé.</li><br/>
+                                <li>si vous utilisez le navigateur Google Chrome<br/>
+                                Cliquez sur l'icône du menu Outils.<br/>
+                                Sélectionnez Options.<br/>
+                                Cliquez sur l'onglet Options avancées et accédez à la section "Confidentialité".<br/>
+                                cliquez sur le bouton Afficher les cookies.<br/>
+                                Repérez les fichiers qui contiennent le nom « www.lanimesalerie » Sélectionnez-les et supprimez-les.<br/>
+                                Cliquez sur » Fermer « pour revenir à votre navigateur</li>
+                            </ol>
+                            <p>Le paramétrage sur une plateforme de gestion des cookies<br/>
+                            Vous pouvez gérer vos cookies en vous rendant sur des plateformes de gestion des cookies proposées par les professionnels de la publicité.</p>
+                            <h3>7 - Dispositions juridictionnelles</h3>
+                            <p>Ce site, ainsi que ses conditions générales d’utilisation, sont soumis au droit français. La présentation des produits est conforme au droit français.<br/>
+                            Même s’il est accessible par d’autres utilisateurs, le Site et son contenu sont destinés à l’accès et l’utilisation des résidents français.<br/>
+                            Par conséquent, en cas de litige, les tribunaux français seront seuls compétents.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+<!-- paw-->
+            <section>
+                <div class="container"> <!-- container ou fluid ? -->
+                    <div class="row"> <!-- align 12 colonnes -->
+                        <div class="col-lg-12 col-sm-12">
+                            <a href="#top"><img src="images/paw.png" alt="image" width="56" height="48" class="img-fluid paw"></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        </main>
+
+        <footer>
+            <?php include("footer.php");?>   
+        </footer>
+
+    </body>
+</html>
