1f947c2f49286a3822ae0b808a0e085835e5f747
KuroeGame <cloe-13500@hotmail.fr>
2021-11-22 09:55:06 +0100

    Create mentionlegales.php

---
+++ mentionlegales.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" type="text/css">
+<!-- Lier bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+
+<!-- Script JS -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+<!-- intégration icon fontawesome -->
+        <script src="https://kit.fontawesome.com/6939106af0.js" crossorigin="anonymous"></script>
+        <title>La Nîmes'alerie</title>
+    </head>
+    <body>
+
+        <header>
+            <?php include("header.php");?>
+        </header>
+
+        <main>
+
+<!-- chemin d'acces-->
+            <section>
+                <div class="container-fluid"> <!-- container ou fluid ? -->
+                    <div class="row"> <!-- align 12 colonnes -->
+                        <div class="col-lg-12 col-sm-12">
+                            <div class="">
+                                <p>
+                                    <a href="index.php">Accueil</a>
+                                    -
+                                    <a href="">Mentions Légales</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+<!-- contenu-->
+            <section>
+                <div class="container"> <!-- container ou fluid ? -->
+                    <h2 class="ms-4 titre">Mentions Légales</h2>
+                    <div class="row"> <!-- align 12 colonnes -->
+                        <div class="col-lg-12 col-sm-12 pt-5 pb-5">
+                            <h3>1 - Édition du site</h3>
+                            <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet <strong>https://lanimesalerie.com/</strong> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:<br/>
+                            <br/>
+                            Propriétaire du site : M. Jean-Baptiste Capard Responsable de La Nîmes'alerie - <strong>Contact :</strong> m.capard@gmail.com 06 06 06 06 06 - <strong>Adresse :</strong> 0 Rue de l'animalerie 30000 Nîmes.<br/>
+                            <br/>
+                            <strong>Identification de l'entreprise :</strong> SARL<br/>
+                            <strong>Entrepreneur individuel :</strong> M. Jean-Baptiste Capard Responsable de La Nîmes'alerie au capital social de 15000€<br/>
+                            <strong>SIREN :</strong> 666 666 666<br/>
+                            <strong>Adresse postale :</strong> 0 Rue de l'animalerie 30000 Nîmes. https://lanimesalerie.com/cgv<br/>
+                            <br/> 
+                            Hébergeur : OVH SAS - 2 rue Kellermann - BP 80157 - 59053 Roubaix Cedex 1 - Téléphone : 1007<br/>
+                            <br/>
+                            Délégué à la protection des données : M.Jean Machin - m.machin@hotmail.fr<br/>
+                            <br/>
+                            Autres contributeurs : Webmaster : KuroeDesign - https://kuroedesign.fr/</p>
+                            <h3>2 - Propriété intellectuelle et contrefaçons</h3>
+                            <p>Entrepreneur individuel : M. Jean-Baptiste Capard Responsable de La Nîmes'alerie est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.<br/>
+                            <br/>
+                            Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Entrepreneur individuel : M. Jean-Baptiste Capard Responsable de La Nîmes'alerie.<br/>
+                            <br/>
+                            Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+                            <h3>3 - Limitations de responsabilité</h3>
+                            <p>Entrepreneur individuel : M. Jean-Baptiste Capard Responsable de La Nîmes'alerie ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site https://lanimesalerie.com/.<br/>
+                            <br/>
+                            Entrepreneur individuel : M. Jean-Baptiste Capard Responsable de La Nîmes'alerie décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur https://lanimesalerie.com/.<br/>
+                            <br/>
+                            Entrepreneur individuel : M. Jean-Baptiste Capard Responsable de La Nîmes'alerie s’engage à sécuriser au mieux le site https://lanimesalerie.com/, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.<br/>
+                            <br/>
+                            Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. Entrepreneur individuel : M. Jean-Baptiste Capard Responsable de La Nîmes'alerie se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données.<br/>
+                            <br/>
+                            Le cas échéant, Entrepreneur individuel : M. Jean-Baptiste Capard Responsable de La Nîmes'alerie se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).</p>
+                            <h3>4 - CNIL et gestion des données personnelles</h3>
+                            <p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site https://lanîmesalerie.com/ dispose d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre Délégué à la Protection des Données : M.Jean Machin - m.machin@hotmail.fr.<br/>
+                            <br/>
+                            Pour plus d'informations sur la façon dont nous traitons vos données (type de données, finalité, destinataire...), lisez notre Politique de Confidentialité.<br/>
+                            <br/>
+                            Il est également possible de déposer une réclamation auprès de la CNIL.</p>
+                            <h3>5 - Liens hypertextes et cookies</h3>
+                            <p>Le site https://lanimesalerie.com/ contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers https://lanimesalerie.com/.<br/>
+                            <br/>
+                            La navigation sur le site https://lanimesalerie.com/ est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.<br/>
+                            <br/>
+                            Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les données ainsi obtenues permettent d'obtenir des mesures de fréquentation, par exemple.<br/>
+                            <br/>
+                            Vous avez la possibilité d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera déposé sans votre consentement.<br/>
+                            <br/>
+                            Les cookies sont enregistrés pour une durée maximale de 6 mois.<br/>
+                            <br/>
+                            Pour plus d'informations sur la façon dont nous faisons usage des cookies, lisez notre Politique de Confidentialité.</p>
+                            <h3>6 - Droit applicable et attribution de juridiction</h3>
+                            <p>Tout litige en relation avec l’utilisation du site https://lanimesalerie.com/ est soumis au droit français. En dehors des cas où la loi ne le permet pas, il est fait attribution exclusive de juridiction aux tribunaux compétents de Nîmes.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+<!-- paw-->
+            <section>
+                <div class="container"> <!-- container ou fluid ? -->
+                    <div class="row"> <!-- align 12 colonnes -->
+                        <div class="col-lg-12 col-sm-12">
+                            <a href="#top"><img src="images/paw.png" alt="image" width="56" height="48" class="img-fluid paw"></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        </main>
+
+        <footer>
+            <?php include("footer.php");?>   
+        </footer>
+
+    </body>
+</html>
